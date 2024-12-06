cf9b7061dbd1191827ec6cf6dd198c86e310bd7f
Manoj Shinde <manojshinde@MacBookAir.lan>
2024-12-06 09:04:15 +0530

    adding skeleton for encryption service

---
+++ encryption-service/src/main/java/com/qsafe/encryption/EncryptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.qsafe.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncryptionServiceApplication.class, args);
+	}
+
+}
---
+++ encryption-service/src/test/java/com/qsafe/encryption/EncryptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qsafe.encryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
